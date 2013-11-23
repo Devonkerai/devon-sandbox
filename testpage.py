749b09fafa43c5d85ba26238617dae817d4adadb
devon.kerai@hotmail.com <d.kerai-11@student.lboro.ac.uk>
2013-11-23 20:11:39 +0000

    code from the tutorial

---
+++ testpage.py
@@ -0,0 +1,10 @@
+# Python test script
+
+print "Hello World\n"
+
+a = int(raw_input("Type in a number: "))
+b = int(raw_input("Type in another number: "))
+c = a * b
+
+print "Resulting number:",c
+
