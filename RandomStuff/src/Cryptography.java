ef5f08399ed9c66dec2e5805008810c769d07dca
Devon Kerai <devon.kerai@betfair.com>
2016-03-06 15:28:54 +0000

    Program to encrypt and decrypt words.

--- RandomStuff/src/Cryptography.java
+++ RandomStuff/src/Cryptography.java
@@ -1,21 +1,56 @@
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Cryptography {
 
+    private static String password = "Bn";
+    private static char[] guess = new char[password.length()];
+    private static StringBuilder encryptedPassword = new StringBuilder();
+    private static char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".toCharArray();
+
     public static void main(String[] args) throws Exception {
+        encryptedPassword = encryptPassword(password);
+        System.out.println("Encoded password of: " + password + " is: " + encryptedPassword);
+        decryptPassword();
+    }
 
-        String password = "helloworld";
-
+    private static StringBuilder encryptPassword(String password) throws NoSuchAlgorithmException {
+        StringBuilder hash = new StringBuilder();
         MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
         messageDigest.update(password.getBytes());
-
         byte byteData[] = messageDigest.digest();
-
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < byteData.length; i++) {
-            stringBuilder.append(Integer.toString(byteData[i] & 0xff + 0x100, 16).substring(1));
+        for (byte aByteData : byteData) {
+            hash.append(Integer.toString(aByteData & 0xff + 0x100, 16).substring(1));
         }
+        return hash;
+    }
 
-        System.out.println("Hex format of password: " + stringBuilder.toString());
+    private static void decryptPassword() throws NoSuchAlgorithmException {
+        for (char anAlphabet : alphabet) {
+            for (char anAlphabet1 : alphabet) {
+                guess[0] = anAlphabet;
+                guess[1] = anAlphabet1;
+                String attempt = printGuess();
+//                    System.out.println("Attempt is: " + attempt);
+                StringBuilder encryptedPasswordAttempt = encryptPassword(attempt);
+//                    System.out.println("Encoded password of: " + attempt + " is: " + encryptedPasswordAttempt);
+                checkPassword(attempt, encryptedPasswordAttempt);
+            }
+        }
+    }
+
+    private static String printGuess() {
+        String guessAsString = "";
+        for (char c : guess) {
+            guessAsString += c;
+        }
+        return guessAsString;
+    }
+
+    private static void checkPassword(String attempt, StringBuilder encryptedPasswordAttempt) {
+        if (encryptedPasswordAttempt.toString().equals(encryptedPassword.toString())) {
+            System.out.println("You have found the password! It is: " + attempt);
+            System.exit(0);
+        }
     }
 }
