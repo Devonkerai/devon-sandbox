e0a09bcd5a4e728398be9dd1b216d625496fa107
Devon Kerai <devon.kerai@betfair.com>
2016-04-03 01:27:15 +0100

    Improved cryptography program so it is scalable.

--- RandomStuff/src/Cryptography.java
+++ RandomStuff/src/Cryptography.java
@@ -1,60 +1,77 @@
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Cryptography {
 
-    private static String password = "Bn";
-    private static char[] guess = new char[password.length()];
-    private static StringBuilder encryptedPassword = new StringBuilder();
-    private static char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".toCharArray();
+    private static String password = "AAB";
+    private static String encryptedPassword;
+    private static char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".toCharArray();
+    private static List<List<String>> prefixBySize = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
         encryptedPassword = encryptPassword(password);
-        System.out.println("Password hash of: " + password + " is: " + encryptedPassword);
+        System.out.println("Your password is: " + password + ". It's hash is: " + encryptedPassword);
         decryptPassword();
     }
 
-    private static StringBuilder encryptPassword(String password) throws NoSuchAlgorithmException {
+    private static String encryptPassword(String password) throws NoSuchAlgorithmException {
         StringBuilder hash = new StringBuilder();
         MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
         messageDigest.update(password.getBytes());
         byte byteData[] = messageDigest.digest();
-        for (byte aByteData : byteData) {
-            hash.append(Integer.toString(aByteData & 0xff + 0x100, 16).substring(1));
+        for (byte aByte : byteData) {
+            hash.append(Integer.toString(aByte & 0xff + 0x100, 16).substring(1));
         }
-        return hash;
+        return hash.toString();
     }
 
     private static void decryptPassword() throws NoSuchAlgorithmException {
-        for (char firstLetter : alphabet) {
-            for (char secondLetter : alphabet) {
-                guess[0] = firstLetter;
-                guess[1] = secondLetter;
-                guessPassword();
+
+
+        prefixBySize.add(Collections.singletonList(""));
+
+        String pass = null;
+        int prefixCounter = 0;
+        while (pass == null) {
+            final List<String> prefixes = prefixBySize.get(prefixCounter);
+            pass = attemptDecrypt(prefixes);
+            prefixCounter++;
+        }
+    }
+
+    private static String attemptDecrypt(List<String> prefixes) throws NoSuchAlgorithmException {
+        List<String> failedAttempts = new ArrayList<>();
+
+        for (String prefix : prefixes) {
+            String newprefix = prefix;
+            for (char c : alphabet) {
+                String tempPass = newprefix += c;
+                if (guessPassword(tempPass)) {
+                    return tempPass;
+                }
+                failedAttempts.add(tempPass);
+                newprefix = prefix;
             }
         }
+        prefixBySize.add(failedAttempts);
+        return null;
     }
 
-    private static void guessPassword() throws NoSuchAlgorithmException {
-        String attempt = printGuess();
-        StringBuilder encryptedPasswordAttempt = encryptPassword(attempt);
-//                    System.out.println("Attempt is: " + attempt);
-//                    System.out.println("Encoded password of: " + attempt + " is: " + encryptedPasswordAttempt);
-        validateHash(attempt, encryptedPasswordAttempt);
-    }
-
-    private static String printGuess() {
-        String guessAsString = "";
-        for (char c : guess) {
-            guessAsString += c;
+    private static boolean guessPassword(String attempt) throws NoSuchAlgorithmException {
+        String encryptedPasswordAttempt = encryptPassword(attempt);
+//        System.out.println("Encoded password of: " + attempt + " is: " + encryptedPasswordAttempt);
+        if (validateHash(encryptedPasswordAttempt)) {
+            System.out.println("You have found the password! It is: " + attempt + ". It has the hash: " + encryptedPasswordAttempt);
+            return true;
         }
-        return guessAsString;
+        return false;
     }
 
-    private static void validateHash(String attempt, StringBuilder encryptedPasswordAttempt) {
-        if (encryptedPasswordAttempt.toString().equals(encryptedPassword.toString())) {
-            System.out.println("You have found the password! It is: " + attempt);
-            System.exit(0);
-        }
+    private static boolean validateHash(String encryptedPasswordAttempt) {
+
+        return encryptedPasswordAttempt.equals(encryptedPassword);
     }
 }
